eadb7d38071b3decaca8fa8e1efbe96316806ce3
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2020-11-24 08:17:12 -0700

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Seeding Music Category and Answers...'
+music = Category.create!(name: 'music')
+puts 'Created Category "music"'
+
+Answer.create!(category: music, term: 'the beatles', variants: ['the beat alls', 'beatles', 'thebeatles', 'george harrison', 'paul mccartney', 'ringo starr', 'john lennon'])
+Answer.create!(category: music, term: 'pink floyd', variants: [])
+Answer.create!(category: music, term: 'daft punk', variants: [])
+Answer.create!(category: music, term: 'keane', variants: ['whiney english band'])
+Answer.create!(category: music, term: 'the lightning seeds', variants: ['whiney english band 2'])
+Answer.create!(category: music, term: 'queen', variants: ['freddie mercury'])
+Answer.create!(category: music, term: 'electric light orchestra', variants: ['elo'])
+Answer.create!(category: music, term: 'the black kids', variants: [])
+Answer.create!(category: music, term: 'psy', variants: ['gangnam style'])
+Answer.create!(category: music, term: 'kevin macleod', variants: [])
+puts 'Created music Answers'
+
+
+puts 'Seeding Video Game Category and Answers...'
+video_games = Category.create!(name: 'video games')
+puts 'Created Category "video games"'
+
+Answer.create!(category: video_games, term: 'half life', variants: ['halflife', 'halflife 2', 'half life 2', 'hl', 'hl2'])
+Answer.create!(category: video_games, term: 'dwarf fortress', variants: ['dorf fortress', 'dwarffortress', 'df'])
+Answer.create!(category: video_games, term: 'spelunky', variants: ['spelunky classic', 'spelunky 1', 'spelunky 2', 'spelunky2', 'spelunky1', 'spelunk'])
+Answer.create!(category: video_games, term: 'starcraft', variants: ['sc', 'star craft', 'sc2', 'star craft 2', 'starcraft 2', 'starcraft2', 'warcraft'])
+Answer.create!(category: video_games, term: 'portal', variants: ['portal 2', 'portal2'])
+Answer.create!(category: video_games, term: 'hotline miami', variants: ['hotline miami 2', 'hotlinemiami', 'hotline miami 2: wrong number'])
+Answer.create!(category: video_games, term: 'super smash brothers', variants: ['ssb', 'brawl', 'melee', 'smash', 'super smash bros'])
+Answer.create!(category: video_games, term: 'stardew valley', variants: ['stardew'])
+Answer.create!(category: video_games, term: 'into the breach', variants: [])
+Answer.create!(category: video_games, term: 'league of legends', variants: ['league', 'lol', 'dota 2', 'smite', 'heroes of the storm', 'hots']) # let's be honest they're all just as terrible and awesome as each other and as heroin
+puts 'Created video games Answers'
